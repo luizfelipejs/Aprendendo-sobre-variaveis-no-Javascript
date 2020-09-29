2a025b175424d6a4a67aed1bfc7c9c4b73ec5d43
Luiz Felipe Pereira bandeira <68253749+luizfelipejs@users.noreply.github.com>
2020-09-29 19:04:19 -0300

    upload example.js

---
+++ exemplos.js
@@ -0,0 +1,11 @@
+// Exemplo de variavel com const 
+const nome = "João Paulo"
+const idade = 19
+
+// exemplo com let 
+let nome2 = "Jo soares"
+let idade2 = 24
+
+// exemplo com var 
+var nome3 = "Baden powell"
+var idade3 = 89
